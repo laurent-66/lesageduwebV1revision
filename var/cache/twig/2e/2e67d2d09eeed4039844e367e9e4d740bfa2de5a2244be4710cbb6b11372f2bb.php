62909abb1e030ec70ddc017377faf9352902d1ca
Laurent LESAGE <laurent.lesage51@gmail.com>
2021-11-30 17:59:04 +0100

    21-11-30 update

---
+++ var/cache/twig/2e/2e67d2d09eeed4039844e367e9e4d740bfa2de5a2244be4710cbb6b11372f2bb.php
@@ -0,0 +1,195 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* training.html.twig */
+class __TwigTemplate_8f858ccfa7b2c21c00801ac3372087709a231e8f57620f56a0ced761eb24fc4f extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "template.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("template.html.twig", "training.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        echo "Formation";
+    }
+
+    // line 5
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        echo " 
+<div class=\"container-fluid\" id=\"training\">
+    <div class=\"container container-training\">
+        <h2> FORMATIONS</h2>
+        <div class=\"divider\"></div>
+            <div class=\"row\">
+                <div class=\"col-12\">
+                    <div class=\"container card-education bg bg-white px-0\">
+                        <div class=\"card-content py-3 px-3\">
+                            <h5>2020- à aujourd'hui</h5>
+                            <div><i class=\"fas fa-user-graduate fa-3x\"></i></div>
+                            <h3>Openclassrooms</h3>
+                            <h4>Alternance<br /> Certification professionnelle de développeur d'application - PHP/symfony</h4>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class=\"row\">
+                <div class=\"col-12\">
+                    <div class=\"container card-education bg bg-white px-0\">
+                        <div class=\"card-content py-3 px-3\">
+                            <h5>2019-2020</h5>
+                            <div><i class=\"fas fa-user-graduate fa-3x\"></i></div>
+                            <h3>Centre de Formation ADRAR</h3>
+                            <h4>Titre professionnel développeur web et web mobile</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class=\"row\">
+                <div class=\"col-12\">
+                    <div class=\"container card-education bg bg-white px-0\">
+                        <div class=\"card-content py-3 px-3\">
+                            <h5>1997-1999</h5>
+                            <div><i class=\"fas fa-user-graduate fa-3x\"></i></div>
+                            <h3>Lycée Dhuoda-Nîmes</h3>
+                            <h4>Brevet de Technicien Supérieur en Bâtiment</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class=\"row mb-3\">
+                <div class=\"col-12\">
+                    <div class=\"container card-education bg bg-white px-0\">
+                        <div class=\"card-content py-3 px-3\">
+                            <h5>1995-1997</h5>
+                            <div><i class=\"fas fa-user-graduate fa-3x\"></i></div>
+                            <h3>Lycée Ste louise de Marillac</h3>
+                            <h4> Bac STI option Génie Civil</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "training.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  54 => 5,  47 => 3,  36 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends \"template.html.twig\" %}
+
+{% block title %}Formation{% endblock %}
+
+{% block body %} 
+<div class=\"container-fluid\" id=\"training\">
+    <div class=\"container container-training\">
+        <h2> FORMATIONS</h2>
+        <div class=\"divider\"></div>
+            <div class=\"row\">
+                <div class=\"col-12\">
+                    <div class=\"container card-education bg bg-white px-0\">
+                        <div class=\"card-content py-3 px-3\">
+                            <h5>2020- à aujourd'hui</h5>
+                            <div><i class=\"fas fa-user-graduate fa-3x\"></i></div>
+                            <h3>Openclassrooms</h3>
+                            <h4>Alternance<br /> Certification professionnelle de développeur d'application - PHP/symfony</h4>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class=\"row\">
+                <div class=\"col-12\">
+                    <div class=\"container card-education bg bg-white px-0\">
+                        <div class=\"card-content py-3 px-3\">
+                            <h5>2019-2020</h5>
+                            <div><i class=\"fas fa-user-graduate fa-3x\"></i></div>
+                            <h3>Centre de Formation ADRAR</h3>
+                            <h4>Titre professionnel développeur web et web mobile</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class=\"row\">
+                <div class=\"col-12\">
+                    <div class=\"container card-education bg bg-white px-0\">
+                        <div class=\"card-content py-3 px-3\">
+                            <h5>1997-1999</h5>
+                            <div><i class=\"fas fa-user-graduate fa-3x\"></i></div>
+                            <h3>Lycée Dhuoda-Nîmes</h3>
+                            <h4>Brevet de Technicien Supérieur en Bâtiment</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class=\"row mb-3\">
+                <div class=\"col-12\">
+                    <div class=\"container card-education bg bg-white px-0\">
+                        <div class=\"card-content py-3 px-3\">
+                            <h5>1995-1997</h5>
+                            <div><i class=\"fas fa-user-graduate fa-3x\"></i></div>
+                            <h3>Lycée Ste louise de Marillac</h3>
+                            <h4> Bac STI option Génie Civil</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+{% endblock %} ", "training.html.twig", "C:\\Users\\laure\\Documents\\ProjectPhp\\Site_portfolio_laurent_V1\\templates\\training.html.twig");
+    }
+}
---
+++ var/cache/twig/f2/f290dc5ffc42cdd19e7c710ea68d9c29497774b3ed933932e4655ae4a493148a.php
@@ -0,0 +1,154 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* blogs-list.html.twig */
+class __TwigTemplate_71f4debf39d511db281fa3a7471a9074e2dcb9303fbae3ab82a4348df1bfadcf extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "template.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("template.html.twig", "blogs-list.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 2
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        echo "Blog";
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        echo " 
+<div class=\"container-fluid mx-0 my-0 px-0 mx-0\" id=\"blogs-list\">
+    <div class=\"heading\">
+        <h2>Liste des blogs</h2>
+    </div>
+    <div class=\"container-blog-list\">
+        ";
+        // line 9
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["blogs"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["blog"]) {
+            // line 10
+            echo "            <div class=\"card-blog\">
+                <div class=\"card-img\">
+                    <img src=\"\\uploads\\";
+            // line 12
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["blog"], "image_blog", [], "any", false, false, false, 12), "html", null, true);
+            echo "\" class=\"img\" alt=\"img\">
+                </div>
+                <div class=\"card-body\">
+                    <h5 class=\"card-title\">";
+            // line 15
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["blog"], "title_blog", [], "any", false, false, false, 15), "html", null, true);
+            echo "</h5>
+                    <p class=\"card-text\">";
+            // line 16
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["blog"], "chapo_blog", [], "any", false, false, false, 16), "html", null, true);
+            echo "</p>
+                    <p>Date: ";
+            // line 17
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["blog"], "last_update_blog", [], "any", false, false, false, 17), "html", null, true);
+            echo "</p>
+                    <p>Auteur: ";
+            // line 18
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["blog"], "pseudo_blog", [], "any", false, false, false, 18), "html", null, true);
+            echo "</p>
+                    <p><a href=";
+            // line 19
+            echo twig_escape_filter($this->env, ("/blogs/" . twig_get_attribute($this->env, $this->source, $context["blog"], "id", [], "any", false, false, false, 19)), "html", null, true);
+            echo " class=\"btn btn-primary\">consulter</a></p>
+                </div>
+            </div>
+        ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['blog'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 23
+        echo "    </div>
+</div>   
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "blogs-list.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  105 => 23,  95 => 19,  91 => 18,  87 => 17,  83 => 16,  79 => 15,  73 => 12,  69 => 10,  65 => 9,  54 => 3,  47 => 2,  36 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends \"template.html.twig\" %}
+{% block title %}Blog{% endblock %}
+{% block body %} 
+<div class=\"container-fluid mx-0 my-0 px-0 mx-0\" id=\"blogs-list\">
+    <div class=\"heading\">
+        <h2>Liste des blogs</h2>
+    </div>
+    <div class=\"container-blog-list\">
+        {% for blog in blogs %}
+            <div class=\"card-blog\">
+                <div class=\"card-img\">
+                    <img src=\"\\uploads\\{{blog.image_blog}}\" class=\"img\" alt=\"img\">
+                </div>
+                <div class=\"card-body\">
+                    <h5 class=\"card-title\">{{blog.title_blog}}</h5>
+                    <p class=\"card-text\">{{blog.chapo_blog}}</p>
+                    <p>Date: {{blog.last_update_blog}}</p>
+                    <p>Auteur: {{blog.pseudo_blog}}</p>
+                    <p><a href={{'/blogs/'~blog.id}} class=\"btn btn-primary\">consulter</a></p>
+                </div>
+            </div>
+        {% endfor %}
+    </div>
+</div>   
+{% endblock %}
+", "blogs-list.html.twig", "C:\\Users\\laure\\Documents\\ProjectPhp\\Site_portfolio_laurent_V1\\templates\\blogs-list.html.twig");
+    }
+}
